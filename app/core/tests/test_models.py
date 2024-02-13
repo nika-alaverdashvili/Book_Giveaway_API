beab7fd772669ac78a4a0df0ced7937ffacba3e2
nikaa <nikoloz.alaverdashvili.1@btu.edu.ge>
2024-02-13 23:54:34 +0400

    fx: lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -22,10 +22,10 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
-            ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
-            ['test4@example.COM', 'test4@example.com'],
+            ('test1@EXAMPLE.com', 'test1@example.com'),
+            ('Test2@Example.com', 'Test2@example.com'),
+            ('TEST3@EXAMPLE.com', 'TEST3@example.com'),
+            ('test4@example.COM', 'test4@example.com'),
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
@@ -45,4 +45,3 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-        
