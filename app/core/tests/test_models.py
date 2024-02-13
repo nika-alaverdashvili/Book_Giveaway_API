b23ff930b9669316883d7116a555b7862e28a241
nikaa <nikoloz.alaverdashvili.1@btu.edu.ge>
2024-02-13 23:51:10 +0400

    fx: lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,6 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     """Test models."""
-
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
