45f0edd502cf3cd8cf2502df3fca54431331a850
nikaa <nikoloz.alaverdashvili.1@btu.edu.ge>
2024-03-03 19:57:41 +0400

    feat: unit tests for book API

--- app/book/tests/test_book_api.py
+++ app/book/tests/test_book_api.py
@@ -1,7 +1,3 @@
-"""
-Tests for book APIs.
-"""
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -15,17 +11,26 @@ from book.serializers import BookSerializer
 BOOKS_URL = reverse('book:book-list')
 
 
+def create_author(name='Test Author'):
+    return Author.objects.create(name=name)
+
+
+def create_genre(name='Test Genre'):
+    return Genre.objects.create(name=name)
+
+
+def create_condition(name='Test Condition'):
+    return Condition.objects.create(name=name)
+
+
 def create_book(user, author, genre, condition, **params):
-    """Create and return a sample book."""
     defaults = {
         'title': 'Sample book title',
         'pickup_location': 'Sample pickup location',
         'is_available': True,
     }
     defaults.update(params)
-
-    book = Book.objects.create(owner=user, author=author, genre=genre, condition=condition, **defaults)
-    return book
+    return Book.objects.create(owner=user, author=author, genre=genre, condition=condition, **defaults)
 
 
 class PublicBookAPITests(TestCase):
@@ -37,22 +42,24 @@ class PublicBookAPITests(TestCase):
     def test_auth_required(self):
         """Test that authentication is required for retrieving books."""
         res = self.client.get(BOOKS_URL)
-
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateBookAPITests(TestCase):
     """Test authenticated book API access."""
 
-    def setUp(self):
-        self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
         )
-        self.author = Author.objects.create(name='Test Author')
-        self.genre = Genre.objects.create(name='Test Genre')
-        self.condition = Condition.objects.create(name='Test Condition')
+        cls.author = create_author()
+        cls.genre = create_genre()
+        cls.condition = create_condition()
+
+    def setUp(self):
+        self.client = APIClient()
         self.client.force_authenticate(self.user)
 
     def test_retrieve_books(self):
@@ -82,3 +89,64 @@ class PrivateBookAPITests(TestCase):
         serializer = BookSerializer(books, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_book(self):
+        """Test creating a new book."""
+        author_data = {'name': 'strihtgng'}
+        genre_data = {'name': 'string'}
+        condition_data = {'name': 'string'}
+        payload = {
+            'title': 'strfrgijhgng',
+            'author': author_data,
+            'genre': genre_data,
+            'condition': condition_data,
+            'pickup_location': 'string',
+            'is_available': True
+        }
+
+        response = self.client.post(BOOKS_URL, payload, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update_book(self):
+        """Test updating a book."""
+        book = create_book(user=self.user, author=self.author, genre=self.genre, condition=self.condition)
+        new_title = 'Updated Book Title'
+        payload = {
+            'title': new_title,
+            'author': {'name': 'New Author Name'},
+            'genre': {'name': 'New Genre Name'},
+            'condition': {'name': 'New Condition Name'},
+            'pickup_location': 'New Pickup Location',
+            'is_available': False
+        }
+
+        url = reverse('book:book-detail', args=[book.id])
+        response = self.client.put(url, payload, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        book.refresh_from_db()
+        self.assertEqual(book.title, new_title)
+
+    def test_partial_update_book(self):
+        """Test partially updating a book."""
+        book = create_book(user=self.user, author=self.author, genre=self.genre, condition=self.condition)
+        new_title = 'Updated Book Title'
+        payload = {'title': new_title}
+
+        url = reverse('book:book-detail', args=[book.id])
+        response = self.client.patch(url, payload, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        book.refresh_from_db()
+        self.assertEqual(book.title, new_title)
+
+    def test_delete_book(self):
+        """Test deleting a book."""
+        book = create_book(user=self.user, author=self.author, genre=self.genre, condition=self.condition)
+
+        url = reverse('book:book-detail', args=[book.id])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Book.objects.filter(id=book.id).exists())
